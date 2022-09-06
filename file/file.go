0cb588c1286ec4c2fc1d8520381f9b9e97c3cc24
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2022-09-06 16:27:53 +0700

    add file util

---
+++ file/file.go
@@ -0,0 +1,48 @@
+package file
+
+import (
+	"errors"
+	"fmt"
+	"mime/multipart"
+	"os"
+
+	stringutil "github.com/cubetiq/cubetiq-utils-go/string"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
+
+func SaveMultipartToFile(c *fiber.Ctx, f *multipart.FileHeader, path string, filename string) *multipart.FileHeader {
+	// check if filename is empty, it will put uuid to filename
+	name := ""
+	if stringutil.IsEmpty(filename) {
+		name = uuid.New().String()
+	} else {
+		name = filename
+	}
+
+	// check if path is empty, it will put ./ in the root directory of project
+	destPath := ""
+	if stringutil.IsEmpty(path) {
+		destPath = "./"
+	} else {
+		destPath = path
+	}
+
+	// get source filename to split a filename and take only extension
+	sourceFileName := f.Filename
+	extension := stringutil.GetPartOfLast(sourceFileName, ".")
+
+	// sum up for destination of file
+	destFile := destPath + "\\" + name + "." + extension
+
+	// check destination of file not exists, it will create a directory
+	if _, err := os.Stat(destPath); errors.Is(err, os.ErrNotExist) {
+		// 0777 => public-read-write
+		os.MkdirAll(destPath, 0777)
+	}
+
+	// save file
+	c.SaveFile(f, fmt.Sprint(destFile))
+
+	return f
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,17 @@ module github.com/cubetiq/cubetiq-utils-go
 go 1.17
 
 require (
+	github.com/gofiber/fiber/v2 v2.37.0
 	github.com/golang-jwt/jwt/v4 v4.4.2
+	github.com/google/uuid v1.3.0
 	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.39.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 )
