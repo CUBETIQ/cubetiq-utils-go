abaec3d84f602b1f664064451bfa2754882415d7
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2022-09-07 17:15:49 +0700

    add generate random string function in string utils

--- string/string.go
+++ string/string.go
@@ -1,6 +1,8 @@
 package string
 
 import (
+	"crypto/rand"
+	"math/big"
 	"strconv"
 	"strings"
 )
@@ -23,4 +25,18 @@ func IsEmpty(s string) bool {
 
 func IsNotEmpty(s string) bool {
 	return s != ""
+}
+
+func GenerateRandomString(n int) (string, error) {
+	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"
+	ret := make([]byte, n)
+	for i := 0; i < n; i++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(letters))))
+		if err != nil {
+			return "", err
+		}
+		ret[i] = letters[num.Int64()]
+	}
+
+	return string(ret), nil
 }
