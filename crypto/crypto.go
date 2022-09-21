08d17caaedb87cf2acc0a65804cb04b719427448
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2022-09-21 16:26:49 +0700

    add crypto utils

---
+++ crypto/crypto.go
@@ -0,0 +1,68 @@
+package crypto
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"io"
+)
+
+func Encrypt(plainData string, s string) (string, error) {
+	secret := []byte(s)
+	if _, err := rand.Read(secret); err != nil {
+		return "", err
+	}
+
+	cipherBlock, err := aes.NewCipher(secret)
+	if err != nil {
+		return "", err
+	}
+
+	aead, err := cipher.NewGCM(cipherBlock)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, aead.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return "", err
+	}
+
+	return base64.URLEncoding.EncodeToString(aead.Seal(nonce, nonce, []byte(plainData), nil)), nil
+}
+
+func Decrypt(encodedData string, s string) (string, error) {
+	secret := []byte(s)
+	if _, err := rand.Read(secret); err != nil {
+		return "", err
+	}
+
+	encryptData, err := base64.URLEncoding.DecodeString(encodedData)
+	if err != nil {
+		return "", err
+	}
+
+	cipherBlock, err := aes.NewCipher(secret)
+	if err != nil {
+		return "", err
+	}
+
+	aead, err := cipher.NewGCM(cipherBlock)
+	if err != nil {
+		return "", err
+	}
+
+	nonceSize := aead.NonceSize()
+	if len(encryptData) < nonceSize {
+		return "", err
+	}
+
+	nonce, cipherText := encryptData[:nonceSize], encryptData[nonceSize:]
+	plainData, err := aead.Open(nil, nonce, cipherText, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plainData), nil
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gofiber/fiber/v2 v2.37.0
 	github.com/golang-jwt/jwt/v4 v4.4.2
 	github.com/google/uuid v1.3.0
+	go.mongodb.org/mongo-driver v1.10.2
 	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8
 )
 
